0be13c465574f2d69eb1aba54b2e31421488540f
trongluan23 <131422480+trongluan23@users.noreply.github.com>
2026-06-07 12:29:19 +0700

    v1.2

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from api import create_app
+import os
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    debug = os.environ.get('FLASK_ENV') != 'production'
+    app.run(debug=debug, host='0.0.0.0', port=port)
